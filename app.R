36092dcf6f58b6ae25d70b185db72333b566a405
Daniel Uni Goseb Produktives <goseberg.d@gmail.com>
2018-01-23 10:41:32 +0100

    First working commit
    TODO: Performance chisquare
    Readme
    Power plots

--- app.R
+++ app.R
@@ -1,14 +1,15 @@
-# This is the samplezir web application made with shiny. You can run the application by clicking
+# This is the samplezir web application made with shiny. 
+# You can run the application by clicking
 # the 'Run App' button above.
-#
-# 
 
 library(shiny)
 library(markdown)
 library(samplesizr)
 
+max_sliders <- 10
+
 ui <- navbarPage("samplesizr!",
-  navbarMenu("Two Groups",
+  navbarMenu("Two groups",
     tabPanel("z Test",
       sidebarLayout(
         sidebarPanel(
@@ -79,7 +80,7 @@ ui <- navbarPage("samplesizr!",
           ),
           sliderInput("t_alpha",
             "Desired Type I Error alpha",
-            min = 0,
+            min = .005,
             max = .2,
             value = .05, 
             step = .005
@@ -87,7 +88,7 @@ ui <- navbarPage("samplesizr!",
           sliderInput("t_pwr",
            "Desired power",
            min = 0.5,
-           max = 1,
+           max = .99,
            value = .8, 
            step = .01
           ),
@@ -103,6 +104,184 @@ ui <- navbarPage("samplesizr!",
           verbatimTextOutput("ttest")
         )
       )
+    ),
+    tabPanel("ANCOVA",
+      sidebarLayout(
+        sidebarPanel(
+          sliderInput("ancova_effect",
+            "Effect size on alternative",
+            min = 0,
+            max = 50, 
+            value = 10, 
+            step = .01
+          ),
+          sliderInput("ancova_corr",
+            "Correlation to covariate C",
+             min = 0,
+             max = 1,
+             value = .3, 
+             step = .01
+          ),
+          sliderInput("ancova_sd",
+            "Standard deviation of sample",
+            min = .01,
+            max = 100,
+            value = 20, 
+            step = .01
+          ),
+          sliderInput("ancova_alpha",
+             "Desired Type I Error alpha",
+             min = .005,
+             max = .2,
+             value = .05, 
+             step = .005
+          ),
+          sliderInput("ancova_pwr",
+            "Desired power",
+            min = 0.5,
+            max = .99,
+            value = .8, 
+            step = .01
+          ),
+          sliderInput("ancova_r",
+            "Desired Allocation",
+            min = 0.1,
+            max = 10,
+            value = 1, 
+            step = .1
+          ),
+          checkboxInput("ancova_gs",
+            "Guenther/Schouten correction",
+            value = TRUE
+          )
+        ),
+        mainPanel(
+          verbatimTextOutput("ancova")
+        )
+      )
+    ),
+    tabPanel("Chi-square Test",
+      sidebarLayout(
+        sidebarPanel(
+          sliderInput("chisq_p_Y",
+            "Event rate of group Y on alternative",
+            min = 0,
+            max = 1,
+            value = .5, 
+            step = .01
+          ),
+          sliderInput("chisq_p_X",
+            "Event rate of group X on alternative",
+            min = 0,
+            max = 1,
+            value = .3, 
+            step = .01
+          ),
+          sliderInput("chisq_alpha",
+            "Desired Type I Error alpha",
+            min = .005,
+            max = .2,
+            value = .05, 
+            step = .005
+          ),
+          sliderInput("chisq_pwr",
+            "Desired power",
+            min = 0.5,
+            max = .99,
+            value = .8, 
+            step = .01
+          ),
+          sliderInput("chisq_r",
+            "Desired Allocation",
+            min = 0.1,
+            max = 10,
+            value = 1, 
+            step = .1
+          ),
+          checkboxInput("chisq_power.exact",
+            "Berechnung zur exakten Power",
+            value = TRUE
+          )
+        ),
+        mainPanel(
+          verbatimTextOutput("chisq")
+        )
+      )
+    )
+  ),
+  navbarMenu("k > 2 groups",
+    tabPanel("f Test",
+      sidebarLayout(
+        sidebarPanel(
+          sliderInput("f_n.groups", 
+            "Number of groups",
+            min = 3,
+            max = 10,
+            value = 3,
+            step = 1
+          ),
+          uiOutput(
+            "f_mu_A"
+          ),
+          sliderInput("f_sd",
+                      "Standard deviation of sample",
+                      min = 0.1,
+                      max = 100,
+                      value = 20, 
+                      step = .01
+          ),
+          sliderInput("f_alpha",
+            "Desired Type I Error alpha",
+            min = .005,
+            max = .2,
+            value = .05, 
+            step = .005
+          ),
+          sliderInput("f_pwr",
+            "Desired power",
+            min = 0.5,
+            max = 0.99,
+            value = .8, 
+            step = .01
+          )
+        ),
+        mainPanel(
+          verbatimTextOutput("f")
+        )
+      )
+    ),
+    tabPanel("Chi-square Test",
+     sidebarLayout(
+       sidebarPanel(
+         sliderInput("chisq_m_n.groups", 
+            "Number of groups",
+            min = 3,
+            max = 10,
+            value = 3,
+            step = 1
+         ),
+         uiOutput(
+           "chisq_m_p_A"
+         ),
+         sliderInput("chisq_m_alpha",
+           "Desired Type I Error alpha",
+           min = .005,
+           max = .2,
+           value = .05, 
+           step = .005
+         ),
+         sliderInput("chisq_m_pwr",
+           "Desired power",
+           min = 0.5,
+           max = 0.99,
+           value = .8, 
+           step = .01
+         )
+       ),
+       mainPanel(
+         verbatimTextOutput("chisq_mult_groups")
+       )
+     )
     )
   ),
   tabPanel("About",
@@ -126,14 +305,14 @@ server <- function(input, output) {
     sd     <- input$z_sd
     r      <- input$z_r
     
-    tmp    <- samplesizr::n_ztest(
+    z_out    <- samplesizr::n_ztest(
       effect = effect,
       sd     = sd,
       alpha  = alpha, 
       power  = power, 
       r      = r
     )
-    print(tmp)
+    print(z_out)
     
   })
   
@@ -145,24 +324,157 @@ server <- function(input, output) {
     sd     <- input$t_sd
     r      <- input$t_r
     
-    tmp    <- samplesizr::n_ttest(
+    t_out    <- samplesizr::n_ttest(
       effect = effect,
       sd     = sd,
       alpha  = alpha, 
       power  = power, 
       r      = r
     )
-    print(tmp)
+    print(t_out)
     
   })
   
-  output$future <- renderText({
-    print("This feature is still at work")
+  output$chisq <- renderPrint({
+    
+    p_Y    <- input$chisq_p_Y
+    p_X    <- input$chisq_p_X
+    alpha  <- input$chisq_alpha
+    power  <- input$chisq_pwr
+    r      <- input$chisq_r
+    power.exact <- input$chisq_power.exact
+    
+    chisq_out  <- samplesizr::n_chisq(
+      p_Y    = p_Y,
+      p_X    = p_X,
+      alpha  = alpha, 
+      power  = power, 
+      r      = r,
+      power.exact = power.exact
+    )
+    print(chisq_out)
+    
   })
   
-  output$table <- DT::renderDataTable({
-    DT::datatable(cars)
+  output$ancova <- renderPrint({
+    
+    effect <- input$ancova_effect
+    corr   <- input$ancova_corr
+    sd     <- input$ancova_sd
+    alpha  <- input$ancova_alpha
+    power  <- input$ancova_pwr
+    r      <- input$ancova_r
+    gs     <- input$ancova_gs
+    
+    ancova_out  <- samplesizr::n_ancova(
+      effect = effect,
+      corr   = corr,
+      sd     = sd,
+      alpha  = alpha,
+      power  = power, 
+      r      = r,
+      gs     = gs
+    )
+    print(ancova_out)
+    
   })
+  
+  output$f <- renderPrint({
+    mu_A <- c()
+    n.groups <- input$f_n.groups
+    sd    <- input$f_sd
+    alpha <- input$f_alpha
+    power <- input$f_pwr
+    
+    for (i in 1:n.groups){
+      mu_A[i] <- eval(parse(text = paste0("input$mu_",i)))
+    }
+    
+    if (any(mu_A != 0)){
+      f_out <-  n_ftest(
+        mu_A     = mu_A,
+        sd       = sd, 
+        n.groups = n.groups,
+        alpha    = alpha,
+        power    = power
+      )
+      print(f_out)
+    } 
+  })
+  
+  output$f_mu_A <- renderUI({
+    f_mu_A_input_list <- lapply(1:input$f_n.groups, function(i) {
+      mu_number_name <- paste("mu_", i, sep="")
+      uiOutput(mu_number_name)
+    } )
+    do.call(tagList, f_mu_A_input_list)
+  })
+  
+  for (i in 1:max_sliders) {
+    local({
+      my_i <- i
+      mu_number_name <- paste("mu_", my_i, sep="")
+      
+      output[[mu_number_name]] <- renderUI({
+        sliderInput(
+          mu_number_name,
+          paste0("Erwarteter Wert Gruppe ", my_i),
+          min = -100,
+          max = 100,
+          value = 0,
+          step = 1
+        )
+      })
+    })
+  }
+  
+  output$chisq_mult_groups <- renderPrint({
+    p_A <- c()
+    n.groups <- input$chisq_m_n.groups
+    alpha <- input$chisq_m_alpha
+    power <- input$chisq_m_pwr
+    
+    for (i in 1:n.groups){
+      p_A[i] <- eval(parse(text = paste0("input$p_",i)))
+    }
+    
+    if (any(p_A != .5)){ 
+      chisq_m_out <- n_chisq_mult_groups(
+        p_A      = p_A,
+        n.groups = n.groups,
+        alpha    = alpha,
+        power    = power
+      )
+      print(chisq_m_out)
+    }
+  })
+  
+  output$chisq_m_p_A <- renderUI({
+    chisq_m_p_A_input_list <- lapply(1:input$chisq_m_n.groups, function(i) {
+      p_number_name <- paste("p_", i, sep="")
+      uiOutput(p_number_name)
+    }
+    )
+    do.call(tagList, chisq_m_p_A_input_list)
+  })
+  
+  for (i in 1:max_sliders) {
+    local({
+      my_i <- i
+      p_number_name <- paste("p_", my_i, sep="")
+      
+      output[[p_number_name]] <- renderUI({
+        sliderInput(
+          p_number_name,
+          paste0("Erwartete Rate Gruppe ", my_i),
+          min = 0,
+          max = 1,
+          value = .5,
+          step = .005
+        )
+      })
+    })
+  }
   
 }
 
