3a6ae5499448e690b6307d77bd43f38e5f9e6cae
Daniel Uni Goseb Produktives <goseberg.d@gmail.com>
2018-01-27 18:51:22 +0100

    refreshed z-test refreshed readme

--- README.md
+++ README.md
@@ -1,9 +1,62 @@
-# samplesizr App
+# samplesizr
+<hr>
 
-This is the samplesizr Web Application made with Shiny. It is based on the Book 
+Hello! Welcome to the samplesizr web application.
 
-> M. Kieser, Fallzahlberechnung in der medizinischen Forschung (2018), 1. Auflage, Springer
+While planning a clinical trial the calculation of the sample size is essential.
+This web application gives you a fast first access to estimate the needed sample
+size for your clinical trial. 
+On the panel above you can choose the statistical test you want to
+perform. Choose the input parameters and press 'Calculate!' to perform the
+sample size calculation.
 
-## Two Group Comparison
+This app cannot replace the judgement and expertise of an biometrician 
+performing a sample size calculation. It should give an orientation and a 
+'feeling' for the power of the certain tests.
 
-## k Groups Comparion
+The theory behind that and much more is explained in [1].
+
+### Example
+
+Imagine a clinical trial with an intervention group and a control group.
+The primary endpoint is the quality of life for cancer patients. The QLQ-C30 questionnaire is used to operationalize this endpoint.
+For a new intervention you expect an improval in quality of life.
+A clinical relevant difference of 10 points on the scale is needed
+to call the new intervention 'better'.
+For the decision for the new intervention, given the intervention is really
+'better' you want to have a minimal probability of 0.9 (power).
+You want a maximal type I error of 0.05 (two-sided) / 0.025 (one-sided).
+You know that the standard deviation will be 20 points.
+Click on the tab 'z-test' above to look up the results!
+
+### About samplesizr
+
+Samplesizr is the shiny web application based on the R package 
+<a href="https://github.com/goseberg/samplesizr">
+samplesizr.
+</a> 
+
+This R package performs sample size calculations based on:
+
+> [1] M. Kieser, Fallzahlberechnung in der medizinischen Forschung (2018), 1th Edition, Springer
+
+The samplesizr is still under development. 
+Now only the z test is implemented with full functionality but soon the other
+tests will follow.
+
+A special thanks to KK for his supervision, patience and help whilst 
+teaching me the tools which made our work on this project very efficient and productive.
+
+A special thanks to MK for his inspiration to this project and teaching of the
+needed theory behind sample size calculation.
+
+A click on the logo will lead you to the Webpage of our Institute.
+
+<a href="https://www.klinikum.uni-heidelberg.de/Homepage-Abteilung.7980.0.html">
+  <img src="imbi.png" alt="IMBI Heidelberg">
+</a>
+
+
+Daniel Goseberg,  
+IMBI Heidelberg,  
+January 27th in Heidelberg
--- app.R
+++ app.R
@@ -13,54 +13,90 @@ ui <- navbarPage("samplesizr",
   tabPanel("Overview",
     includeMarkdown("README.md")
   ),
-  tabPanel("Z-Test",
+  tabPanel("z-Test",#===========================================================
+    h2("Sample size calculation for the two-sided z-test"),
     sidebarLayout(
       sidebarPanel(
-        actionButton("z_button", "Update output"),
-        h3("Assumptions:"),
-        tags$div(title = "Supercoole Erklärung!",
+        actionButton("z_button", "Calculate!", width = '100%'),
+        tags$hr(),
+        h4("Data assumptions"),
+        tags$div(title = "Value of primary endpoint (control group)",
           numericInput("z_mean_x", label = h5("Mean of control group"), 
-                       value = 0, step = 0.025)
+                       value = 60, step = 0.025)
         ),
-        numericInput("z_mean_y", label = h5("Mean of intervention group"), 
-                     value = 0.3, step = 0.025), 
-        numericInput("z_sd_y_and_x", label = h5("Standard deviation in both group"), 
-                     value = 0.3, step = 0.025), 
-        # TODO: verify that values are sensible / minimium differenc of ??? 
-        tags$br(),
-        h3("Operatic characteristics:"),
-        sliderInput("z_alpha",
-          "Maximal type I error rate",
-          min = .005,
-          max = .2,
-          value = .05, 
-          step = .005
+        tags$div(title = "Value of primary endpoint (intervention group). See NOTE.",
+          numericInput("z_mean_y", label = h5("Mean of intervention group"), 
+                     value = 70, step = 0.025)
         ),
-        sliderInput("z_pwr",
-          "Desired power",
-          min = 0.5,
-          max = .99,
-          value = .8, 
-          step = .01
+        p("NOTE: Specify the minimal clinical significance to make you call
+          the intervention better regarding the primary endpoint."),
+        tags$div(title = "Equal for both groups",
+          numericInput("z_sd_y_and_x", 
+            label = h5("Standard deviation in both groups"), 
+            value = 20, step = 0.025)
         ),
-        tags$br(),
-        h3("Other:"),
-        sliderInput("z_r",
-          "Desired Allocation",
-          min = 0.1,
-          max = 10,
-          value = 1, 
-          step = .1
+        tags$hr(),
+        h4("Type I/II Error characteristics"),
+        sliderInput("z_alpha", "Maximal type I error rate",
+          min = .005, max = .2, value = .05, step = .005
         ),
-        tags$br()
+        sliderInput("z_pwr", "Desired power",
+          min = 0.5, max = .99, value = .9, step = .01
+        ),
+        tags$hr(),
+        h4("Other"),
+        tags$div(title = "Ratio of group sizes (larger group) : (smaller group)",
+          sliderInput("z_r", "Desired allocation r",
+            min = 1, max = 10, value = 1, step = 1
+          )
+        ),
+        tags$div(title = "Group which has more patients",
+          radioButtons("z_r_x_or_y", "Direction of allocation",  
+            choiceNames = c("Intervention", "Control"), choiceValues = c(1,2)
+          )
+        )
       ),
       mainPanel(
+        tags$div(title = "[1] M. K., Fallzahlberechnung in der medizinischen 
+          Forschung (2018), 1th Edition, Springer",
+          p("The methods used for calculation are explained on the pages
+            13 - 16 in [1]."
+          )
+        ),
+        tags$hr(),
+        p("The null hypothesis: control and 
+          intervention group have the same mean in the primary endpoint."),
+        p("The alternative hypothesis: There is a 
+          difference between the two groups in the primary endpoint."),
+        p("Please specify the difference you
+          think to observe in your data assumptions."),
+        h4("Data assumptions"),
         plotOutput("z_assumptions_plot"),
-        verbatimTextOutput("ztest")
+        p("Plot 1. The densities for the
+          distribution assumptions of the Input data are shown."
+        ),
+        tags$hr(),
+        h3("Results"),
+        verbatimTextOutput("ztest"),
+        tags$br(),
+        h4("Test statistic"),
+        plotOutput("z_density_plot"),
+        p("Plot 2. The density function of the Z statistic is shown. 
+          The vertical line marks the border between a decision for the 
+          hypothesis/alternative."
+        ),
+        tags$br(),
+        h4("Power curve"),
+        plotOutput("z_effect_plot"),
+        p("Plot 3. The dependency on the mean difference is shown.
+          Note that the sample size is fixed. To see the sample size
+          look in the results. The lines mark the effect size where 
+          the desired power is reached."
+        )
       )
     )
   ),
-  tabPanel("t Test",
+  tabPanel("t Test",#===========================================================
     sidebarLayout(
       sidebarPanel(
         sliderInput("t_effect",
@@ -104,7 +140,7 @@ ui <- navbarPage("samplesizr",
       )
     )
   ),
-  tabPanel("ANCOVA",
+  tabPanel("ANCOVA",#===========================================================
     sidebarLayout(
       sidebarPanel(
         sliderInput("ancova_effect",
@@ -159,7 +195,7 @@ ui <- navbarPage("samplesizr",
       )
     )
   ),
-  tabPanel("Chi-square Test",
+  tabPanel("Chi-square Test",#==================================================
     sidebarLayout(
       sidebarPanel(
         sliderInput("chisq_p_Y",
@@ -247,7 +283,7 @@ ui <- navbarPage("samplesizr",
       )
     )
   ),
-  tabPanel("Chi-square Test",
+  tabPanel("Chi-square Test",#==================================================
    sidebarLayout(
      sidebarPanel(
        sliderInput("chisq_m_n.groups", 
@@ -286,8 +322,6 @@ ui <- navbarPage("samplesizr",
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
-  
-  
 # Z-Test =======================================================================
   z_input <- reactive({
     effect <- input$z_mean_y - input$z_mean_x
@@ -295,42 +329,47 @@ server <- function(input, output) {
     power  <- input$z_pwr
     sd     <- input$z_sd_y_and_x
     r      <- input$z_r
+    r_inv  <- input$z_r_x_or_y
     
-    std_effect <- effect/sd
-    if (abs(std_effect) < .05) {
+    effect_sd <- effect/sd
+    
+    if (r_inv == 2) { r <- (1/r) }
+    
+    if (abs(effect_sd) < .05) {
       showModal(modalDialog(
-        title = "Important message",
-        "This is an important message!"
+        title = "Problem",
+        "Please specify a larger group difference!"
+      ))
+    } else {
+      return(list(r = r, alpha = alpha, power = power, sd = sd, 
+        mu_x = input$z_mean_x, mu_y = input$z_mean_y
       ))
     }
-    
-    return(list(r = r, alpha = alpha, power = power, sd = sd, 
-                mu_x = input$z_mean_x, mu_y = input$z_mean_y
-              )
-    )
   })
   
   z_input_lazy <- reactive({
     input$z_button
+    
     isolate(z_input())
   })
   
-  output$ztest <- renderPrint({
+  z_output <- reactive({
     
     input$z_button
-    tmp <- isolate(z_input())
     
-    z_out    <- samplesizr::n_ztest(
-      effect = abs(tmp$mu_y - tmp$mu_x),
-      sd     = tmp$sd,
-      alpha  = tmp$alpha, 
-      power  = tmp$power, 
-      r      = tmp$r
+    z_in  <- z_input_lazy()
+    
+    z_out <- n_ztest(
+      effect = z_in$mu_y - z_in$mu_x,
+      sd     = z_in$sd,
+      alpha  = z_in$alpha, 
+      power  = z_in$power, 
+      r      = z_in$r
     )
-    
-    print(z_out)
-    
+    return(z_out)
   })
+  
+  output$ztest <- renderPrint({ print(z_output()) })
   
   output$z_assumptions_plot <- renderPlot({
     
@@ -348,9 +387,82 @@ server <- function(input, output) {
     mutate(
       PDF = dnorm(x, mean, sd)
     ) %>%
-      ggplot(aes(x, PDF, color = group)) + geom_line()
+      ggplot(aes(x, PDF, color = group)) + geom_line() + theme_classic()
     
   })
+  
+  output$z_density_plot <- renderPlot({
+    
+    z_out <- z_output()
+    
+    effect <- z_out$effect
+    n_X    <- z_out$n_X
+    r      <- z_out$r
+    sd     <- z_out$sd
+    power  <- z_out$power_out
+    
+    nc <-  sqrt( (r/(1+r)) * n_X ) * (effect / sd)
+    z_beta  <- qnorm(power)
+    if (effect < 0) { x_vline <- nc + z_beta }
+    if (effect > 0) { x_vline <- nc - z_beta }
+    min_x <- floor( min(0, nc) - 4 )
+    max_x <- ceiling( max(0, nc) + 4 )
+    
+    data_frame(
+      z = rep(seq(min_x, max_x, length.out = 101), 2),
+      group = c(rep("Hypothesis", 101), rep("Alternative", 101)),
+      mean = c(rep(0, 101), rep(nc, 101)),
+      sd = 1
+    ) %>%
+      mutate(
+        PDF = dnorm(z, mean, sd)
+      ) %>%
+      ggplot(aes(z, PDF, color = group)) +
+      geom_line()  +
+      geom_vline(xintercept = x_vline, lty = 3) +
+      theme_classic() +
+      scale_color_discrete(name="Test statistic Z")
+  })
+  
+  output$z_effect_plot <- renderPlot({
+    
+    z_out <- z_output()
+    
+    effect <- z_out$effect
+    n_X    <- z_out$n_X
+    alpha  <- z_out$alpha
+    r      <- z_out$r
+    sd     <- z_out$sd
+    power  <- z_out$power
+    
+    min_x <- 0
+    max_x <- effect + power * effect
+    x = seq(from = min_x, to = max_x, length.out = 101)
+    
+    f <- function(x) {power_ztest(effect = x, sd=sd, n_X=n_X, alpha = alpha, r = r)}
+    
+    vert_line <- x[which.min(abs(f(x) - power))]
+    vert_tick <- round(vert_line, 3)
+    
+    data_frame(
+      x = x,
+      y = f(x)
+    ) %>%
+    ggplot(aes(x, y)) + geom_line() + 
+      theme_classic() +
+      geom_hline(yintercept = power, lty = 2) +
+      geom_vline(xintercept = vert_line, lty = 3) +
+      scale_y_continuous(
+        name = "Power", 
+        breaks = c(0, .5, power, 1)
+      ) + 
+      scale_x_continuous(
+        name   = "Effect", 
+        breaks = c(min_x, effect, max_x)
+      )
+    
+  })
+  
   
   
   
@@ -395,6 +507,8 @@ server <- function(input, output) {
     
   })
   
+# ANCOVA =======================================================================
+  
   output$ancova <- renderPrint({
     
     effect <- input$ancova_effect
@@ -418,6 +532,8 @@ server <- function(input, output) {
     
   })
   
+# F TEST =======================================================================
+  
   output$f <- renderPrint({
     mu_A <- c()
     n.groups <- input$f_n.groups
@@ -440,7 +556,7 @@ server <- function(input, output) {
       print(f_out)
     } 
   })
-  
+
   output$f_mu_A <- renderUI({
     f_mu_A_input_list <- lapply(1:input$f_n.groups, function(i) {
       mu_number_name <- paste("mu_", i, sep="")
@@ -466,6 +582,8 @@ server <- function(input, output) {
       })
     })
   }
+  
+# Chisquare mult. groups =======================================================
   
   output$chisq_mult_groups <- renderPrint({
     p_A <- c()
